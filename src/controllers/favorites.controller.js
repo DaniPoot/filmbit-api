59e41f2fcad3ecfa4eab07c4b581eeea45d8bd14
DaniPoot <danielpoot.uc2@gmail.com>
2021-06-27 18:27:24 -0500

    Added funcionality for every route

--- src/controllers/favorites.controller.js
+++ src/controllers/favorites.controller.js
@@ -6,8 +6,15 @@ const Favorite = require("../models/favorite.js");
  * res = Status 200 junto con la info de abajo.
  * Devolver listado de ids de las peliculas.
  */
-const getAllFavorites = (req, res) => {};
-
+const getAllFavorites = (req, res) => {
+  const { user_id } = req.body;
+  try {
+    const favorite = Favorite.findAll({ where: { user_id } });
+    return res.status(200).json(favorite);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+};
 
 /*
  * URL = userId, movieId
@@ -15,23 +22,54 @@ const getAllFavorites = (req, res) => {};
  * res = Status 200 junto con la info de abajo.
  * Devolver id de la pelicula, si no existe devolver json vacio.
  */
-const getFavorite = (req, res) => {};
+const getFavorite = (req, res) => {
+  const { user_id, movie_id } = req.body;
+  try {
+    const favorite = Favorite.findOne({ where: { user_id, movie_id } });
+    return res.status(200).json(favorite);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+};
 
 /*
  * req.body = {userId, movieId}
- * funcionalidad: validar que no hayan paramatros undefined, validar que no existe 
+ * funcionalidad: validar que no hayan paramatros undefined, validar que no existe
  * un registro con ambos paramentros y crear registro.
- * res = Status 200 si se creó correctamente. Status 400 si un parametro 
+ * res = Status 200 si se creó correctamente. Status 400 si un parametro
  * no llega o si ya existe un registro.
  */
-const addNewFavorite = (req, res) => {};
+const addNewFavorite = (req, res) => {
+  const { user_id, movie_id } = req.body;
+
+  try {
+    const favorite = Favorite.create({ user_id, movie_id });
+    return res.status(200).json(favorite);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+};
 
 /*
  * req.body = {id}
  * funcionalidad: eliminar el registro según el id de la pelicula.
  * res = Status 400 si un parametro no llega, Status 200 si se elimina el registro correctamente.
  */
-const deleteFavorite = (req, res) => {};
+const deleteFavorite = (req, res) => {
+  const { user_id, movie_id } = req.body;
+
+  try {
+    const favorite = Favorite.destroy({
+      where: {
+        user_id,
+        movie_id,
+      },
+    });
+    return res.status(200).json(favorite);
+  } catch (err) {
+    return res.status(500).json({ error: err });
+  }
+};
 
 module.exports = {
   getAllFavorites,
