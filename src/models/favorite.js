f6fcf8f478cc64df38de2206d75f370ec9be3e3a
DaniPoot <danielpoot.uc2@gmail.com>
2021-06-28 00:03:46 -0500

    Update module

--- src/models/favorite.js
+++ src/models/favorite.js
@@ -1,15 +1,22 @@
 const { DataTypes } = require("sequelize");
 const db = require("../config/db.js");
 
-const Favorites = db.define("favorites_list", {
-  // id_user: {
-  //   type: DataTypes.INTEGER(11),
-  //   allowNull: false,
-  // },
-  id_movie: {
-    type: DataTypes.INTEGER(11),
-    allowNull: false,
+const Favorites = db.define(
+  "favorites_list",
+  {
+    id_user: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+    },
+    id_movie: {
+      type: DataTypes.INTEGER(11),
+      allowNull: false,
+    },
   },
-});
+  {
+    tableName: "favorites_list",
+  }
+);
 
+Favorites.removeAttribute("id");
 module.exports = Favorites;
